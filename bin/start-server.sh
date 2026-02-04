b2df67cae03b148e1501ed33b08ddb79d90ae934
Ibrahim Boudaouara <ibrahim.boudaouara@ec.gc.ca>
2026-02-04 22:29:02 +0000

    Moved export PYTHONPATH & TODO.md replaced by issues

--- bin/start-server.sh
+++ bin/start-server.sh
@@ -6,6 +6,8 @@ project_dir=$(cd -P $(dirname ${BASH_SOURCE[0]})/.. && pwd)
 
 cd ${project_dir}/pygeoapi
 
+export PYTHONPATH=$PYTHONPATH:$ROOT_DIR/share/rpn-pygeoapi # to access custom providers
+
 export PYGEOAPI_CONFIG=${project_dir}/share/rpn-pygeoapi/example-config.yml
 export PYGEOAPI_OPENAPI=example-openapi.yml
 
--- setup.sh
+++ setup.sh
@@ -12,5 +12,3 @@ if [ ! -f "${VENV_PATH}" ] ; then
 fi
 
 source ${VENV_PATH}
-
-export PYTHONPATH=$PYTHONPATH:$ROOT_DIR/share/rpn-pygeoapi
