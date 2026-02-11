f0fe85343fa927ac7d178cba69f55ecd739a90f3
Ibrahim Boudaouara <ibrahim.boudaouara@ec.gc.ca>
2026-02-11 22:27:51 +0000

    feat: add storage symlink to prevent quota overflow

--- initial-setup.sh
+++ initial-setup.sh
@@ -23,3 +23,20 @@ if [ ! -d "${zarr_folder}" ] ; then
 else
     echo "Sample data already linked: '${zarr_folder}'."
 fi
+
+# Local Storage Space
+storage_folder="data/storage"
+
+if [ ! -d "${storage_folder}" ] ; then
+
+    read -p "The storage space is not linked yet. Do you want to create the link now? (y/n): " answer
+
+    if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+        ln -s "/fs/site5/eccc/mrd/rpnsi/$USER" "${storage_folder}"
+        echo "Sucessfully linked storage space: '${storage_folder}'."
+    else
+        echo "Storage link creation skipped."
+    fi
+else
+    echo "Storage space already linked: '${storage_folder}'."
+fi
