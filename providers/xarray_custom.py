767be2f3e5384ce2a9b8f0d773e3ebe88217768b
brenda keimgoutchio <brt004@mrd-geomet-dev.collab.science.gc.ca>
2026-08-14 21:37:56 +0000

    feat: expose all manifest variables in coverage

--- providers/xarray_custom.py
+++ providers/xarray_custom.py
@@ -39,22 +39,20 @@ class CustomXarrayProvider(XarrayProvider):
                 consolidated=False,
             )
 
-            LOGGER.warning("Dataset opened successfully")
+            LOGGER.warning(
+                "Dataset opened successfully"
+            )
 
             LOGGER.warning(
                 "data_vars = %s",
-                list(self._data.data_vars)[:20]
+                list(self._data.data_vars)
             )
 
 
-            #
-            # Sélection automatique d'une variable 
-            #
-
             variables = [
-                v
-                for v in self._data.data_vars
-                if not v.startswith("record")
+                variable
+                for variable in self._data.data_vars
+                if not variable.startswith("record_")
             ]
 
             if not variables:
@@ -62,63 +60,105 @@ class CustomXarrayProvider(XarrayProvider):
                     "No scientific variable found in dataset"
                 )
 
-
-            self.variable = variables[0]
-
             LOGGER.warning(
-                "Selected variable: %s",
-                self.variable
+                "Scientific variables: %s",
+                variables
             )
 
+            self._fields = {}
 
-            da = self._data[self.variable]
+            first_da = None
 
-            LOGGER.warning(
-                "Variable dimensions: %s",
-                da.dims
-            )
+            for variable_name in variables:
 
+                da = self._data[variable_name]
 
-            #
-            # Détection  des axes
-            #
+                LOGGER.warning(
+                    "Processing variable: %s",
+                    variable_name
+                )
 
-            if len(da.dims) < 2:
-                raise RuntimeError(
-                    "Variable does not have enough dimensions"
+                LOGGER.warning(
+                    "Variable dimensions: %s",
+                    da.dims
+                )
+
+                LOGGER.warning(
+                    "Variable shape: %s",
+                    da.shape
+                )
+
+                if len(da.dims) < 2:
+                    LOGGER.warning(
+                        "Skipping variable %s: not enough dimensions",
+                        variable_name
+                    )
+                    continue
+
+                x_field = da.dims[0]
+                y_field = da.dims[1]
+
+                LOGGER.warning(
+                    "%s: x_field=%s y_field=%s",
+                    variable_name,
+                    x_field,
+                    y_field
+                )
+
+                coordinates = {
+                    x_field: range(
+                        da.sizes[x_field]
+                    ),
+                    y_field: range(
+                        da.sizes[y_field]
+                    ),
+                }
+
+                da = da.assign_coords(coordinates)
+
+                self._data[variable_name] = da
+
+                LOGGER.warning(
+                    "%s: Coordinates created: %s",
+                    variable_name,
+                    list(da.coords)
                 )
 
 
-            self.x_field = da.dims[0]
-            self.y_field = da.dims[1]
+                if first_da is None:
+                    first_da = da
+                    self.variable = variable_name
+                    self.x_field = x_field
+                    self.y_field = y_field
 
+                self._fields[variable_name] = {
+                    "type": "float",
+                    "x-ogc-unit": "1"
+                }
 
-            LOGGER.warning(
-                "x_field=%s y_field=%s",
-                self.x_field,
-                self.y_field
-            )
+            if first_da is None:
+                raise RuntimeError(
+                    "No valid scientific variable found in dataset"
+                )
 
-
-            #
-            # Attributs attendus par XarrayProvider
-            #
 
             self.time_field = None
-
             self.storage_crs = None
 
-            shape = da.shape
+            shape = first_da.shape
 
-           
             self._coverage_properties = {
                 "width": shape[0],
                 "height": shape[1],
                 "depth": shape[2] if len(shape) > 2 else 1,
 
-                
                 "resx": 1.0,
                 "resy": 1.0,
+
+                "crs_type": "GeographicCRS",
+                "bbox_crs": (
+                    "http://www.opengis.net/def/crs/OGC/1.3/CRS84"
+                ),
 
                 "axes": {
                     "x": {
@@ -130,24 +170,17 @@ class CustomXarrayProvider(XarrayProvider):
                 }
             }
 
-
             self.axes = self._coverage_properties["axes"]
 
-
-            
-
-            self._fields = {
-                self.variable: {
-                    "type": "float"
-                }
-            }
-
+            LOGGER.warning(
+                "Coverage properties = %s",
+                self._coverage_properties
+            )
 
             LOGGER.warning(
                 "Fields initialized: %s",
                 self._fields
             )
-
 
         else:
             super().__init__(provider_def)
